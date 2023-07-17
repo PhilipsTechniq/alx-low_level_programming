4f651edaeeb56e808c48c7ff18974dfbeae330b5
Philip Sunday <philipdata73@gmail.com>
2023-07-17 07:49:22 -0700

    prints all possible different combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * mail - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		for (x - '0'; x <= '9'; x++)
+		{
+			if (!((x == y) || (y > x)))
+			{
+				putchar(y);
+				putchar(x);
+				if (!(x == '9' && y == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
