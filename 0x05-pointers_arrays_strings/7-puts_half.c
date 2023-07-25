ad91d7f0375295c93be1c42470c37bed56dbcbec
Philip Sunday <philipdata73@gmail.com>
2023-07-24 17:02:27 -0700

    elements of an array of integers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@ void puts_half(char *str)
 {
 	int i, j, k = 0;
 
-	for (i = 0; str[i] != '\n'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		k++;
 	j = (k - 1) / 2;
 	for (i = j + 1; str[i] != '\0'; i++)
