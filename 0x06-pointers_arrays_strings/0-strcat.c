f74171d442152fee85b8903952634ddc0d88e433
Philip Sunday <philipdata73@gmail.com>
2023-07-27 10:35:59 -0700

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - Concatenates two strings
+ * @dest: input value
+ * @src: value to be input
+ * Return: desk
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *concent = dest;
+
+	while (*concent != '\0')
+	{
+		concent++;
+	}
+	while (*src != '\0')
+	{
+		*concent = *src;
+		concent++;
+		src++;
+	}
+	*concent = '\0';
+	return (dest);
+}
