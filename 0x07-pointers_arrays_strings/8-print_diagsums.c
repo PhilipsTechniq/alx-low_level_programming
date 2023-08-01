7b0e9ee3c83a2261b0ecab6bd7993550aefa761f
Philip Sunday <philipdata73@gmail.com>
2023-07-31 20:19:05 -0700

    the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - the sum of the two diagonals
+ * @a: array sum
+ * @size: size of a sqaure
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, y;
+
+	i = 0;
+	j = 0;
+
+	for (y = 0; y < size; y++)
+	{
+		i = i + j;
+	}
+	return (0);
+}
