85c968a62fa66d05d206091da2d0667749635b8e
Philip Sunday <philipdata73@gmail.com>
2023-08-16 17:32:00 -0700

    searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,14 +8,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (size <= 0)
+		return (-1);
+	while (i < size)
 	{
-		if (cmp(array[i]) != 0)
+		if (cmp(array[i]) == 1)
 			return (i);
+		i++;
 	}
-	return (-i);
+	return (-1);
 }
