6cafac8142042a4b83fe7925512ecb77bd0991ac
Philip Sunday <philipdata73@gmail.com>
2023-08-17 06:12:15 -0700

    all file combine

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+* op_add - return sum of two numbers
+* @a: num1
+* @b: num2
+* Return: 0
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - return the difference of two numbers
+* @a: num1
+* @b: num2
+* Return: void
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - return product of two numbers
+* @a: num1
+* @b: num2
+* Return: void
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - divide two numbers
+* @a: num1
+* @b: num2
+* Return: result
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - a division of two numbers
+* @a: num1
+* @b: num2
+* Return: remainder
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
